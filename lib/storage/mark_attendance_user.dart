16a70d55f1af0e988e150e5fb2098538caceb822
UMChannar <usamuhammadchannar@gmail.com>
2023-04-29 13:46:27 +0500

    Attendance marking logic done

---
+++ lib/storage/mark_attendance_user.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+Future<void> markAttendece(String atten, BuildContext context) async {
+  final user = FirebaseAuth.instance.currentUser;
+  if (user == null) {
+    return;
+  }
+
+  final userRef = FirebaseFirestore.instance.collection('users').doc(user.uid);
+  final userDoc = await userRef.get();
+
+  if (userDoc.exists) {
+    final currentDate = DateTime.now().toLocal().toString().substring(0, 10);
+    final attendanceRef = userRef.collection('attendances');
+    final attendanceQuery =
+        await attendanceRef.where('date', isEqualTo: currentDate).get();
+    if (attendanceQuery.docs.isNotEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Attendance Already Marked'),
+          duration: Duration(seconds: 1),
+        ),
+      );
+      return;
+    } else {
+      await attendanceRef.doc().set(
+        {
+          'date': currentDate,
+          'attendance': atten,
+        },
+      );
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Attendance Marked'),
+          duration: Duration(seconds: 1),
+        ),
+      );
+    }
+  }
+}
